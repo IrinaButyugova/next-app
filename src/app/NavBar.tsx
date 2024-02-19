d9bcf8d0ccc0eebca83cb43daa93b231a796f74a
IrinaButyugova <irina.butyugova@sharp-dev.net>
2024-02-19 16:29:19 +0300

    Styling the Active Link

--- src/app/NavBar.tsx
+++ src/app/NavBar.tsx
@@ -1,8 +1,13 @@
+'use client'
+
+import classNames from 'classnames'
 import Link from 'next/link'
+import { usePathname } from 'next/navigation'
 import React from 'react'
 import { AiFillBug } from 'react-icons/ai'
 
 const NavBar = () => {
+    const currentPath = usePathname();
 
     const links= [ 
     {
@@ -19,8 +24,13 @@ const NavBar = () => {
         <ul className='flex space-x-6'>
             {links.map(link => 
                 <Link key={link.href} href={link.href} 
-                className='text-zinc-500 hover:text-zinc-800 transition-colors'>
-                {link.label}</Link>
+                className={classNames({
+                   'text-zinc-900': link.href === currentPath,
+                   'text-zinc-500': link.href !== currentPath,
+                   'hover:text-zinc-800 transition-colors': true
+                })}>
+                    {link.label}
+                </Link>
                 )}
         </ul>
     </nav>
